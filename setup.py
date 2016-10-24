a39912b29ec530372f295292e732c4724f46edb3
quynhdo <quynhdtn.hut@gmail.com>
2016-10-24 13:19:38 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='coreNN',
+    version='1.0',
+    packages=['liir', 'liir.ml', 'liir.ml.core', 'liir.ml.core.layers', 'liir.ml.core.models', 'utils', 'optimizer'],
+    url='',
+    license='',
+    author='quynhdo',
+    author_email='quynhdtn.hut@gmail.com',
+    description='An implementation of core classes for neural networks'
+)
